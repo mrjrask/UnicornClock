8e2f20b31a4a11a38da6078d0f394e4f027f0871
mrjrask <96755551+mrjrask@users.noreply.github.com>
2026-01-12 22:16:03 -0600

    Handle missing font characters

--- unicornclock/fontdriver.py
+++ unicornclock/fontdriver.py
@@ -44,8 +44,14 @@ class FontDriver:
         - Size (the full size of the character)
         """
         offset = 0
+        space_dims = self.chars_font_bounds.get(' ')
         for i, char in enumerate(text):
-            dims = self.chars_font_bounds[char]
+            if char not in self.chars_font_bounds:
+                if space_dims is None:
+                    continue
+                dims = space_dims
+            else:
+                dims = self.chars_font_bounds[char]
 
             # The `+1` is because we come from position to have a width
             character_width = dims[1] - dims[0] + 1
@@ -63,11 +69,10 @@ class FontDriver:
 
     def write_char(self, char, x, y=0):
         char = str(char)
-
-        try:
-            self.font[str(char)]
-        except KeyError:
-            raise Exception("Character '%s' not found in font." % char)
+        if char not in self.font:
+            if ' ' not in self.font:
+                return
+            char = ' '
 
         start, _ = self.chars_font_bounds[char]
         for (px, py) in self.iter_pixel(char):
